a259c094e75b1bb7cfff254c47d464defad5b52b
ddryo <dd.bocsfcryo@gmail.com>
2021-08-10 16:19:26 +0900

    allowed_block_types_all調整

--- lib/gutenberg/block_control.php
+++ lib/gutenberg/block_control.php
@@ -27,15 +27,17 @@ add_filter( 'widget_types_to_hide_from_legacy_widget_block', function( $widget_t
 add_filter( 'allowed_block_types_all', __NAMESPACE__ . '\limit_block_types', 99, 2 );
 function limit_block_types( $allowed_block_types, $block_editor_context ) {
 
-	// 全ブロック取得
-	$all_blocks  = [];
-	$block_types = \WP_Block_Type_Registry::get_instance()->get_all_registered();
-	foreach ( $block_types as $block_type ) {
-		$all_blocks[] = $block_type->name;
+	$allowed_blocks = [];
+	if ( is_array( $allowed_block_types ) && ! empty( $allowed_block_types ) ) {
+		// すでに許可リストがある場合
+		$allowed_blocks = $allowed_block_types;
+	} else {
+		// まだ空だったら全ブロック取得
+		$block_types = \WP_Block_Type_Registry::get_instance()->get_all_registered();
+		foreach ( $block_types as $block_type ) {
+			$allowed_blocks[] = $block_type->name;
+		}
 	}
-
-	// return ['core/legacy-widget', 'core/group' ];
-	// exit;
 
 	$FSE_blocks = [
 		'core/loginout',
@@ -73,7 +75,7 @@ function limit_block_types( $allowed_block_types, $block_editor_context ) {
 		$disallowed_blocks = $FSE_blocks;
 	}
 
-	$allowed_blocks = array_diff( $all_blocks, $disallowed_blocks );
+	$allowed_blocks = array_diff( $allowed_blocks, $disallowed_blocks );
 	$allowed_blocks = array_values( $allowed_blocks ); // array_valuesちゃんとしないと効かない
 	return $allowed_blocks;
 }
