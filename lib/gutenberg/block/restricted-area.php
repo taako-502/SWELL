5e7d45d6596efd01ca4217b59b55895495eb1335
Tetsuaki Hamano / 浜野 哲明 <tetsuaki.hamano@gmail.com>
2021-08-26 15:17:33 +0000

    制限エリアブロック：基本完成

--- lib/gutenberg/block/restricted-area.php
+++ lib/gutenberg/block/restricted-area.php
@@ -21,22 +21,37 @@ register_block_type_from_metadata( T_DIRE . '/src/gutenberg/blocks/restricted-ar
 ] );
 
 function cb_restricted_area( $attrs, $content ) {
+	// ログイン制限有効
+	if ( $attrs['isRole'] ) {
+		// 非ログインユーザー制限あり、かつログイン中は非表示
+		if ( ! $attrs['isLoggedIn'] && is_user_logged_in() ) return '';
 
-	if ( $attrs['nonLoggedin'] && is_user_logged_in() ) return '';
+		// ログインユーザー制限あり
+		if ( $attrs['isLoggedIn'] ) {
+			$allowed_roles = array_keys( array_filter( $attrs['roles'], function( $role ) {
+				return $role === true;
+			}));
 
-	if ( ! is_user_logged_in() ) return '';
-
-	foreach ( [
-		'administrator',
-		'editor',
-		'author',
-		'contributor',
-		'subscriber',
-	] as $role ) {
-		if ( $attrs[ $role ] && current_user_can( $role ) ) {
-			return $content;
+			// 現在のユーザーの権限が制限対象として含まれているか
+			$show_flg = false;
+			foreach ( $allowed_roles as $allowed_role ) {
+				if ( current_user_can( $allowed_role ) ) {
+					$show_flg = true;
+				}
+			}
+			if ( ! $show_flg  ) return '';
 		}
 	}
 
-	return '';
+	// 日時範囲制限有効
+	if ( $attrs['isDateTime'] ) {
+		$current_timestamp = current_time('timestamp', true);
+		$start_timestamp   = isset( $attrs['startDateTime'] ) ? strtotime( $attrs['startDateTime'] ) : null;
+		$end_timestamp     = isset( $attrs['endDateTime'] ) ? strtotime( $attrs['endDateTime'] ) : null;
+
+		if ( $start_timestamp && $current_timestamp < $start_timestamp ) return '';
+		if ( $end_timestamp && $end_timestamp < $current_timestamp ) return '';
+	}
+
+	return $content;
 }
--- src/gutenberg/blocks/restricted-area/index.js
+++ src/gutenberg/blocks/restricted-area/index.js
@@ -1,7 +1,7 @@
 /**
  * @WordPress dependencies
  */
-import { memo } from '@wordpress/element';
+import { useState } from '@wordpress/element';
 import { registerBlockType } from '@wordpress/blocks';
 import {
 	InnerBlocks,
@@ -9,7 +9,16 @@ import {
 	useBlockProps,
 	__experimentalUseInnerBlocksProps as useInnerBlocksProps,
 } from '@wordpress/block-editor';
-import { BaseControl, PanelBody, ToggleControl, CheckboxControl } from '@wordpress/components';
+import {
+	BaseControl,
+	PanelBody,
+	ToggleControl,
+	CheckboxControl,
+	RadioControl,
+	Button,
+	DateTimePicker,
+	Popover,
+} from '@wordpress/components';
 
 /**
  * @SWELL dependencies
@@ -18,58 +27,44 @@ import metadata from './block.json';
 import blockIcon from './_icon';
 import getBlockIcon from '@swell-guten/utils/getBlockIcon';
 
+/**
+ * 設定
+ */
+const loggedInStatuses = [
+	{
+		label: '非ログインユーザー',
+		value: 'noLoggedIn',
+	},
+	{
+		label: 'ログインユーザー',
+		value: 'loggedIn',
+	},
+];
+
+const targetRoles = [
+	{
+		label: '管理者',
+		value: 'administrator',
+	},
+	{
+		label: '編集者',
+		value: 'editor',
+	},
+	{
+		label: '投稿者',
+		value: 'author',
+	},
+	{
+		label: '寄稿者',
+		value: 'contributor',
+	},
+	{
+		label: '購読者',
+		value: 'subscriber',
+	},
+];
+
 const TEMPLATE = [['core/paragraph']];
-
-const SettingArea = memo(({ attributes, setAttributes }) => {
-	const { administrator, editor, author, contributor, subscriber, nonLoggedin } = attributes;
-
-	return (
-		<InspectorControls>
-			<PanelBody title='表示の制限' initialOpen={true}>
-				<BaseControl className='checkbox_group'>
-					<BaseControl.VisualLabel>
-						{'コンテンツを見ることのできるユーザー'}
-					</BaseControl.VisualLabel>
-					<ToggleControl
-						label='非ログインユーザー'
-						checked={nonLoggedin}
-						onChange={(val) => setAttributes({ nonLoggedin: val })}
-					/>
-					<div data-swl-disable={nonLoggedin || null}>
-						<CheckboxControl
-							label='管理者'
-							checked={administrator}
-							onChange={(val) => setAttributes({ administrator: val })}
-						/>
-						<CheckboxControl
-							label='編集者'
-							checked={editor}
-							onChange={(val) => setAttributes({ editor: val })}
-						/>
-						<CheckboxControl
-							label='投稿者'
-							checked={author}
-							onChange={(val) => setAttributes({ author: val })}
-						/>
-						<CheckboxControl
-							label='寄稿者'
-							checked={contributor}
-							onChange={(val) => setAttributes({ contributor: val })}
-						/>
-						<CheckboxControl
-							label='購読者'
-							checked={subscriber}
-							onChange={(val) => setAttributes({ subscriber: val })}
-						/>
-					</div>
-				</BaseControl>
-				<BaseControl>
-					<BaseControl.VisualLabel>{'日時範囲指定'}</BaseControl.VisualLabel>
-				</BaseControl>
-			</PanelBody>
-		</InspectorControls>
-	);
-});
 
 /**
  * 制限エリア
@@ -78,6 +73,12 @@ const blockName = 'swell-block-restrictedArea';
 registerBlockType(metadata.name, {
 	icon: getBlockIcon(blockIcon),
 	edit: ({ attributes, setAttributes }) => {
+		const { roles, isRole, isLoggedIn, isDateTime, startDateTime, endDateTime } = attributes;
+
+		// DateTimePickerのステート
+		const [isOpenStartDateTime, setIsOpenStartDateTime] = useState(false);
+		const [isOpenEndDateTime, setIsOpenEndDateTime] = useState(false);
+
 		// ブロックprops
 		const blockProps = useBlockProps({
 			className: `${blockName} swl-inner-blocks swl-has-margin--s`,
@@ -90,7 +91,103 @@ registerBlockType(metadata.name, {
 
 		return (
 			<>
-				<SettingArea {...{ attributes, setAttributes }} />
+				<InspectorControls>
+					<PanelBody title='表示の制限' initialOpen={true}>
+						<ToggleControl
+							label='ログイン制限を有効にする'
+							checked={isRole}
+							onChange={(val) => setAttributes({ isRole: val })}
+						/>
+						{isRole && (
+							<>
+								<RadioControl
+									label='コンテンツを見ることのできるユーザー'
+									selected={isLoggedIn ? 'loggedIn' : 'noLoggedIn'}
+									options={loggedInStatuses}
+									onChange={(val) => {
+										setAttributes({ isLoggedIn: 'loggedIn' === val });
+									}}
+								/>
+								{isLoggedIn && (
+									<BaseControl className='checkbox_group'>
+										{targetRoles.map((targetRole) => {
+											return (
+												<CheckboxControl
+													label={targetRole.label}
+													key={`key_${targetRole.value}`}
+													checked={roles[targetRole.value]}
+													onChange={(val) => {
+														setAttributes({
+															roles: {
+																...roles,
+																[targetRole.value]: val,
+															},
+														});
+													}}
+												/>
+											);
+										})}
+									</BaseControl>
+								)}
+							</>
+						)}
+						<ToggleControl
+							label='日時範囲制限を有効にする'
+							checked={isDateTime}
+							onChange={(val) => setAttributes({ isDateTime: val })}
+						/>
+						{isDateTime && (
+							<BaseControl>
+								<Button
+									isTertiary
+									onClick={() => {
+										setIsOpenStartDateTime(!isOpenStartDateTime);
+									}}
+								>
+									{isOpenStartDateTime && (
+										<Popover
+											onFocusOutside={() => {
+												setIsOpenStartDateTime(false);
+											}}
+										>
+											<DateTimePicker
+												currentDate={startDateTime}
+												onChange={(val) => {
+													setAttributes({ startDateTime: val });
+													setIsOpenStartDateTime(false);
+												}}
+											/>
+										</Popover>
+									)}
+									{startDateTime ? startDateTime : '開始日時'}
+								</Button>
+								<Button
+									isTertiary
+									onClick={() => {
+										setIsOpenEndDateTime(!isOpenEndDateTime);
+									}}
+								>
+									{isOpenEndDateTime && (
+										<Popover
+											onFocusOutside={() => {
+												setIsOpenEndDateTime(false);
+											}}
+										>
+											<DateTimePicker
+												currentDate={endDateTime}
+												onChange={(val) => {
+													setAttributes({ endDateTime: val });
+													setIsOpenEndDateTime(false);
+												}}
+											/>
+										</Popover>
+									)}
+									{endDateTime ? endDateTime : '終了日時'}
+								</Button>
+							</BaseControl>
+						)}
+					</PanelBody>
+				</InspectorControls>
 				<div {...innerBlocksProps} />
 			</>
 		);
