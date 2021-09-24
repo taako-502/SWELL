63adfbfa23dbfddeb5a3fd42d75d5b4af5a175f2
ddryo <dd.bocsfcryo@gmail.com>
2021-09-24 22:24:13 +0900

    不要になった機能を削除

--- src/gutenberg/blocks/link-list/_sidebar.js
+++ src/gutenberg/blocks/link-list/_sidebar.js
@@ -1,24 +1,13 @@
 /**
  * @WordPress dependencies
  */
-import { MediaUpload, MediaUploadCheck } from '@wordpress/block-editor';
-import {
-	PanelBody,
-	ColorPalette,
-	ButtonGroup,
-	Button,
-	TextControl,
-	BaseControl,
-	RadioControl,
-	ToggleControl,
-} from '@wordpress/components';
+import { PanelBody, ButtonGroup, Button, TextControl, BaseControl } from '@wordpress/components';
 import { useCallback } from '@wordpress/element';
 
 /**
  * @SWELL dependencies
  */
 import SwellIconPicker from '@swell-guten/components/SwellIconPicker';
-import setBlankRel from '@swell-guten/utils/setBlankRel';
 
 /**
  * 設定
@@ -46,18 +35,6 @@ const icons = [
 	'icon-check',
 ];
 
-// アイキャッチの並び順
-const orderCatchOptions = [
-	{
-		label: '画像 → テキスト',
-		value: 'media-text',
-	},
-	{
-		label: 'テキスト → 画像',
-		value: 'text-media',
-	},
-];
-
 // リストスタイル
 const typeList = [
 	{
@@ -70,27 +47,8 @@ const typeList = [
 	},
 ];
 
-// ボタンカラー
-const btnColorSet = [
-	{ name: '赤', color: 'red' },
-	{ name: '青', color: 'blue' },
-	{ name: '緑', color: 'green' },
-];
-
 export default ({ attributes, setAttributes }) => {
-	const {
-		catchOrder,
-		catchImageUrl,
-		catchImageID,
-		listType,
-		listIconPos,
-		listIconName,
-		btnIconPos,
-		btnIconName,
-		btnColor,
-		btnIsNewTab,
-		btnRel,
-	} = attributes;
+	const { listType, listIconPos, listIconName } = attributes;
 
 	// リスト項目のアイコン更新
 	const onlistIconClick = useCallback((val, isSelected) => {
@@ -101,83 +59,8 @@ export default ({ attributes, setAttributes }) => {
 		}
 	}, []);
 
-	// ボタンアイコン更新
-	const onBtnIconClick = useCallback((val, isSelected) => {
-		if (isSelected) {
-			setAttributes({ btnIconName: '' });
-		} else {
-			setAttributes({ btnIconName: val });
-		}
-	}, []);
-
 	return (
 		<>
-			<PanelBody title='アイキャッチ設定' initialOpen={false}>
-				<RadioControl
-					label='並び順'
-					selected={catchOrder}
-					options={orderCatchOptions}
-					onChange={(val) => {
-						setAttributes({ catchOrder: val });
-					}}
-				/>
-				<TextControl
-					className='u-mb-5'
-					label='画像URL'
-					value={catchImageUrl}
-					placeholder='URLを直接入力できます'
-					onChange={(val) => {
-						if ('' === val) {
-							// 画像削除されたら
-							setAttributes({ catchImageUrl: '' });
-						} else {
-							setAttributes({ catchImageUrl: val });
-						}
-					}}
-				/>
-				<div className='swl-btns--media u-mb-20'>
-					<MediaUploadCheck>
-						<MediaUpload
-							onSelect={(media) => {
-								// 画像がなければ
-								if (!media || !media.url) {
-									setAttributes({
-										catchImageUrl: '',
-										catchImageID: 0,
-										catchImageAlt: '',
-									});
-									return;
-								}
-								setAttributes({
-									catchImageUrl: media.url,
-									catchImageID: media.id,
-									catchImageAlt: media.alt,
-								});
-							}}
-							allowedTypes={'image'}
-							value={catchImageID}
-							render={({ open }) => (
-								<Button isPrimary onClick={open}>
-									メディアから選択
-								</Button>
-							)}
-						/>
-					</MediaUploadCheck>
-					<Button
-						isSecondary
-						className='swl-btn--delete'
-						onClick={() => {
-							setAttributes({
-								catchImageUrl: '',
-								catchImageID: 0,
-								catchImageAlt: '',
-							});
-						}}
-					>
-						削除
-					</Button>
-				</div>
-			</PanelBody>
 			<PanelBody title='リスト設定' initialOpen={false}>
 				<BaseControl>
 					<BaseControl.VisualLabel>リストタイプ</BaseControl.VisualLabel>
@@ -233,76 +116,6 @@ export default ({ attributes, setAttributes }) => {
 						</ButtonGroup>
 					</BaseControl>
 				)}
-			</PanelBody>
-			<PanelBody title='ボタン設定' initialOpen={false}>
-				<BaseControl>
-					<BaseControl.VisualLabel>アイコン設定</BaseControl.VisualLabel>
-					<SwellIconPicker
-						icons={icons}
-						iconName={btnIconName}
-						onClick={onBtnIconClick}
-					/>
-					<TextControl
-						label='アイコンのクラス'
-						help='※ Font Awesome のアイコンも一部利用可能です。(svgで出力されるため、cssなどの読み込み不要で使えます)'
-						value={btnIconName}
-						onChange={(val) => {
-							setAttributes({ btnIconName: val });
-						}}
-					/>
-				</BaseControl>
-				{btnIconName && (
-					<BaseControl>
-						<BaseControl.VisualLabel>アイコンの位置</BaseControl.VisualLabel>
-						<ButtonGroup>
-							{iconPosOptions.map((pos) => {
-								const isSelected = pos.val === btnIconPos;
-								return (
-									<Button
-										isSecondary={!isSelected}
-										isPrimary={isSelected}
-										onClick={() => setAttributes({ btnIconPos: pos.val })}
-										key={`key_link_list_icon_pos_${pos.val}`}
-									>
-										{pos.label}
-									</Button>
-								);
-							})}
-						</ButtonGroup>
-					</BaseControl>
-				)}
-				<BaseControl>
-					<BaseControl.VisualLabel>カラー設定</BaseControl.VisualLabel>
-					<ColorPalette
-						value={btnColor}
-						colors={btnColorSet}
-						disableCustomColors={true}
-						onChange={(val) => {
-							setAttributes({ btnColor: val });
-						}}
-					/>
-				</BaseControl>
-				<BaseControl>
-					<BaseControl.VisualLabel>リンク設定</BaseControl.VisualLabel>
-					<ToggleControl
-						label='新しいタブで開く'
-						checked={btnIsNewTab}
-						onChange={(value) => {
-							const newRel = setBlankRel(value, btnRel);
-							setAttributes({
-								btnIsNewTab: value,
-								btnRel: newRel,
-							});
-						}}
-					/>
-					<TextControl
-						label='リンク rel 属性'
-						value={btnRel || ''}
-						onChange={(value) => {
-							setAttributes({ btnRel: value });
-						}}
-					/>
-				</BaseControl>
 			</PanelBody>
 		</>
 	);
--- src/gutenberg/blocks/link-list/index.js
+++ src/gutenberg/blocks/link-list/index.js
@@ -2,13 +2,7 @@
  * @WordPress dependencies
  */
 import { registerBlockType } from '@wordpress/blocks';
-import {
-	InspectorControls,
-	RichText,
-	URLInput,
-	MediaPlaceholder,
-	useBlockProps,
-} from '@wordpress/block-editor';
+import { InspectorControls, RichText, useBlockProps } from '@wordpress/block-editor';
 import { useState, useEffect } from '@wordpress/element';
 
 /**
@@ -33,19 +27,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 const blockName = 'swell-block-linkList';
 registerBlockType(metadata.name, {
 	icon: getBlockIcon(blockIcon),
-	edit: (props) => {
-		const { attributes, setAttributes, isSelected } = props;
-		const {
-			catchOrder,
-			catchImageUrl,
-			catchTitle,
-			btnIconPos,
-			btnIconName,
-			btnText,
-			btnColor,
-			btnHrefUrl,
-		} = attributes;
-
+	edit: ({ attributes, setAttributes, isSelected }) => {
 		// リスト項目のポップアップ
 		const [isItemControlOpen, setItemControlOpen] = useState(false);
 
@@ -66,40 +48,6 @@ registerBlockType(metadata.name, {
 			setItemControlOpen(false);
 		}, [actItem]);
 
-		// タイトル
-		const CatchTtl = () => {
-			return (
-				<RichText
-					placeholder='タイトル...'
-					tagName='div'
-					className={`${blockName}__title`}
-					value={catchTitle}
-					onChange={(value) => {
-						setAttributes({ catchTitle: value });
-					}}
-					onClick={() => {
-						setActItem(undefined);
-						setFocusBtn(false);
-					}}
-				/>
-			);
-		};
-
-		// アイコン
-		const IconTag = ({ iconName }) => {
-			if (!iconName) return null;
-			const iconData = sliceIconData(iconName);
-			if (typeof iconData === 'string') {
-				return <i className={iconName + ' __icon'}></i>;
-			}
-			return <FontAwesomeIcon icon={iconData} className='__icon' />;
-		};
-
-		// ボタン用クラス
-		const btnClass = classnames(`${blockName}__btn`, {
-			[`-icon-pos-${btnIconPos}`]: !!btnIconPos && !!btnIconName,
-		});
-
 		// ブロックprops
 		const blockProps = useBlockProps({
 			className: blockName,
@@ -111,122 +59,26 @@ registerBlockType(metadata.name, {
 					<LinkListSidebar {...{ attributes, setAttributes }} />
 				</InspectorControls>
 				<div {...blockProps}>
-					<div className={`${blockName}__inner`}>
-						<div className={`${blockName}__catch`}>
-							{catchOrder === 'text-media' && <CatchTtl />}
-							<figure className={`${blockName}__figure`}>
-								{!catchImageUrl ? (
-									<MediaPlaceholder
-										labels='画像'
-										onSelect={(media) => {
-											setAttributes({
-												catchImageUrl: media.url,
-												catchImageID: media.id,
-												catchImageAlt: media.alt,
-											});
-										}}
-										onSelectURL={(newURL) => {
-											setAttributes({
-												catchImageUrl: newURL,
-												catchImageID: 0,
-												catchImageAlt: '',
-											});
-										}}
-										accept='image/*'
-										allowedTypes={['image']}
-									/>
-								) : (
-									<img
-										src={catchImageUrl}
-										className={`${blockName}__img`}
-										alt=''
-									/>
-								)}
-							</figure>
-							{catchOrder === 'media-text' && <CatchTtl />}
-						</div>
-						<List
-							{...{
-								blockName,
-								isSelected,
-								attributes,
-								setAttributes,
-								actItem,
-								setActItem,
-								setFocusBtn,
-								isItemControlOpen,
-								setItemControlOpen,
-							}}
-						/>
-						<a
-							className={btnClass}
-							href='###'
-							onClick={() => {
-								setFocusBtn(true);
-								setActItem(undefined);
-							}}
-						>
-							{btnIconPos === 'left' && <IconTag iconName={btnIconName} />}
-							<RichText
-								placeholder='テキスト...'
-								tagName='span'
-								value={btnText}
-								onChange={(value) => {
-									setAttributes({ btnText: value });
-								}}
-							/>
-							{btnIconPos === 'right' && <IconTag iconName={btnIconName} />}
-						</a>
-						{isFocusBtn && (
-							<div className='swl-input--url'>
-								<span className='__label'>href = </span>
-								<URLInput
-									className='__input'
-									value={btnHrefUrl}
-									onChange={(url) => {
-										setAttributes({ btnHrefUrl: url });
-									}}
-									disableSuggestions={!isSelected}
-									isFullWidth
-								/>
-							</div>
-						)}
-					</div>
+					<List
+						{...{
+							blockName,
+							isSelected,
+							attributes,
+							setAttributes,
+							actItem,
+							setActItem,
+							setFocusBtn,
+							isItemControlOpen,
+							setItemControlOpen,
+						}}
+					/>
 				</div>
 			</>
 		);
 	},
 
 	save: ({ attributes }) => {
-		const {
-			catchOrder,
-			catchImageUrl,
-			catchImageID,
-			catchImageAlt,
-			catchTitle,
-			linkList,
-			listType,
-			listIconPos,
-			listIconName,
-			btnIconPos,
-			btnIconName,
-			btnText,
-			btnColor,
-			btnHrefUrl,
-			btnIsNewTab,
-			btnRel,
-		} = attributes;
-
-		// タイトル
-		const CatchTtl = () => {
-			return (
-				<RichText.Content
-					tagName='div'
-					className={`${blockName}__title`}
-					value={catchTitle}
-				/>
-			);
-		};
+		const { linkList, listType, listIconPos, listIconName } = attributes;
 
 		// アイコン
 		const IconTag = ({ iconName }) => {
@@ -237,16 +89,6 @@ registerBlockType(metadata.name, {
 			}
 			return <FontAwesomeIcon icon={iconData} className='__icon' />;
 		};
-
-		// 画像用クラス名
-		const catchImgClass = classnames(`${blockName}__img`, {
-			[`wp-image-${catchImageID}`]: catchImageID,
-		});
-
-		// ボタン用クラス
-		const btnClass = classnames(`${blockName}__btn`, {
-			[`-icon-pos-${btnIconPos}`]: !!btnIconPos && !!btnIconName,
-		});
 
 		// リスト用クラス
 		const listClass = classnames(`${blockName}__list`, {
@@ -262,59 +104,30 @@ registerBlockType(metadata.name, {
 		/* eslint react/jsx-no-target-blank: 0 */
 		return (
 			<div {...blockProps}>
-				<div className={`${blockName}__inner`}>
-					{(catchTitle || catchImageUrl) && (
-						<div className={`${blockName}__catch`}>
-							{catchTitle && catchOrder === 'text-media' && <CatchTtl />}
-							{catchImageUrl && (
-								<figure className={`${blockName}__figure`}>
-									<img
-										src={catchImageUrl}
-										className={catchImgClass}
-										alt={catchImageAlt}
-									/>
-								</figure>
-							)}
-							{catchTitle && catchOrder === 'media-text' && <CatchTtl />}
-						</div>
-					)}
-					{linkList.length > 0 && (
-						<ul className={listClass}>
-							{linkList.map((item, idx) => {
-								return (
-									<li className={`${blockName}__item`} key={idx}>
-										<a
-											className={`${blockName}__link`}
-											href={item.url}
-											target={item.target}
-											rel={item.rel || null}
-										>
-											{listIconPos === 'left' && (
-												<IconTag iconName={listIconName} />
-											)}
-											<RichText.Content tagName='span' value={item.text} />
-											{listIconPos === 'right' && (
-												<IconTag iconName={listIconName} />
-											)}
-										</a>
-									</li>
-								);
-							})}
-						</ul>
-					)}
-					{btnText && (
-						<a
-							className={btnClass}
-							href={btnHrefUrl}
-							target={btnIsNewTab ? '_blank' : null}
-							rel={btnRel || null}
-						>
-							{btnIconPos === 'left' && <IconTag iconName={btnIconName} />}
-							<RichText.Content tagName='span' value={btnText} />
-							{btnIconPos === 'right' && <IconTag iconName={btnIconName} />}
-						</a>
-					)}
-				</div>
+				{linkList.length > 0 && (
+					<ul className={listClass}>
+						{linkList.map((item, idx) => {
+							return (
+								<li className={`${blockName}__item`} key={idx}>
+									<a
+										className={`${blockName}__link`}
+										href={item.url}
+										target={item.target}
+										rel={item.rel || null}
+									>
+										{listIconPos === 'left' && (
+											<IconTag iconName={listIconName} />
+										)}
+										<RichText.Content tagName='span' value={item.text} />
+										{listIconPos === 'right' && (
+											<IconTag iconName={listIconName} />
+										)}
+									</a>
+								</li>
+							);
+						})}
+					</ul>
+				)}
 			</div>
 		);
 	},
