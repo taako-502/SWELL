aad6fee6ffec3e185a7d5310f74e9414704c5d6c
ddryo <dd.bocsfcryo@gmail.com>
2021-10-16 22:57:20 +0900

    調整

--- lib/menu/swell_menu_balloon.php
+++ lib/menu/swell_menu_balloon.php
@@ -3,11 +3,13 @@
 use \SWELL_THEME\Admin_Menu;
 if ( ! defined( 'ABSPATH' ) ) exit;
 
-// 新ふきだし用テーブルがあるかどうか
-$table_name   = \SWELL_Theme::DB_TABLES['balloon'];
-$table_exists = \SWELL_Theme::check_table_exists( $table_name );
-// 旧ふきだし用データがあるかどうか
+$table_exists = \SWELL_Theme::check_table_exists( \SWELL_Theme::DB_TABLES['balloon'] );
 $has_old_data = \SWELL_Theme::has_old_balloon_data();
 
+// 古いデータもなく、新しいテーブルもない場合はアクセス時に作成
+if ( ! $has_old_data && ! $table_exists ) {
+	\SWELL_Theme::create_balloon_table();
+}
+
 ?>
-<div id="swell_setting_page" class="swl-setting -balloon" data-old="<?=esc_attr( $has_old_data || ! $table_exists )?>"></div>
+<div id="swell_setting_page" class="swl-setting -balloon" data-old="<?=esc_attr( $has_old_data )?>"></div>
